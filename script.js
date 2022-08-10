c16838249a9aa130f4b77a3cbf5c0b7852d81040
Juan David Sandoval Salvador <sandovaldavid2201@gmail.com>
2022-08-10 15:40:58 -0500

    clase 04 Concluida

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const btn =document.querySelector('[data-form-btn]');
+( () => {const btn =document.querySelector('[data-form-btn]');
 
 const createTasck = (evento)=>{
     evento.preventDefault();
@@ -9,14 +9,33 @@ const createTasck = (evento)=>{
     task.classList.add("card")
     input.value = '';
     //backticks
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("span");
+    titleTask.classList.add('task')
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
     const content = `
-        <div>
-            <i class="far fa-check-square icon"></i>
-            <span class="task">${value}</span>
-        </div>
         <i class="fas fa-trash-alt trashIcon icon"></i>`;
-    task.innerHTML = content;
+    //task.innerHTML = content;
+    task.appendChild(taskContent);
     list.appendChild(task);
 };
 //arrow functions o funciones anonimas
 btn.addEventListener('click',createTasck);
+
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add('far', 'fa-check-square', 'icon');
+    i.addEventListener('click', completeTask);
+    return i;
+}
+//Immediately invoked funtion expression IIFE
+const completeTask = (evento) => {
+    const element = evento.target;
+    element.classList.toggle('fas');
+    element.classList.toggle('completeIcon');
+    element.classList.toggle('far');
+    //toggle: verifica si una clase exciste, si es asi la quita, caso contrario la agrega.
+}
+})();
