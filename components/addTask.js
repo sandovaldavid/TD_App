c34637049625dab11cf6cf99a827921909beec10
sandovaldavid <sandovaldavid2201@gmail.com>
2025-04-24 23:44:57 -0500

    🎨 format code

--- components/addTask.js
+++ components/addTask.js
@@ -1,61 +1,61 @@
-import checkComplete from "./checkComplete.js";
-import deleteIcon from "./deleteIcon.js";
-import { displayTasks } from "./readTasks.js";
+import checkComplete from './checkComplete.js';
+import deleteIcon from './deleteIcon.js';
+import { displayTasks } from './readTasks.js';
 
 export const addTask = (event) => {
-    event.preventDefault();
-    const list = document.querySelector("[data-list]");
-    const input = document.querySelector("[data-form-input]");
-    const calendar = document.querySelector("[data-form-date]");
+	event.preventDefault();
+	const list = document.querySelector('[data-list]');
+	const input = document.querySelector('[data-form-input]');
+	const calendar = document.querySelector('[data-form-date]');
 
-    const value = input.value;
-    const date = calendar.value;
-    const dateFormat = moment(date).format("DD/MM/YYYY"); //libreria moment
-    if (value == "" || date == "") {
-        return;
-    }
+	const value = input.value;
+	const date = calendar.value;
+	const dateFormat = moment(date).format('DD/MM/YYYY'); //libreria moment
+	if (value == '' || date == '') {
+		return;
+	}
 
-    input.value = "";
-    calendar.value = "";
+	input.value = '';
+	calendar.value = '';
 
-    const complete = false;
+	const complete = false;
 
-    const taskObj = {
-        value,
-        dateFormat,
-        complete,
-        id: uuid.v4(),
-    };
-    list.innerHTML = "";
-    const taskList = JSON.parse(localStorage.getItem("tasks")) || [];
-    taskList.push(taskObj);
-    localStorage.setItem("tasks", JSON.stringify(taskList));
-    displayTasks();
+	const taskObj = {
+		value,
+		dateFormat,
+		complete,
+		id: uuid.v4(),
+	};
+	list.innerHTML = '';
+	const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+	taskList.push(taskObj);
+	localStorage.setItem('tasks', JSON.stringify(taskList));
+	displayTasks();
 };
 
 export const createTasck = ({ value, dateFormat, complete, id }) => {
-    const task = document.createElement("li");
-    task.classList.add("card");
-    //backticks
-    const taskContent = document.createElement("div");
+	const task = document.createElement('li');
+	task.classList.add('card');
+	//backticks
+	const taskContent = document.createElement('div');
 
-    const check = checkComplete(id);
+	const check = checkComplete(id);
 
-    if (complete) {
-        check.classList.toggle("fas");
-        check.classList.toggle("completeIcon");
-        check.classList.toggle("far");
-    }
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
-    titleTask.innerText = value;
-    taskContent.appendChild(check);
-    taskContent.appendChild(titleTask);
-    //task.innerHTML = content;
-    const dateElement = document.createElement("spam");
-    dateElement.innerHTML = dateFormat;
-    task.appendChild(taskContent);
-    task.appendChild(dateElement);
-    task.appendChild(deleteIcon(id));
-    return task;
+	if (complete) {
+		check.classList.toggle('fas');
+		check.classList.toggle('completeIcon');
+		check.classList.toggle('far');
+	}
+	const titleTask = document.createElement('span');
+	titleTask.classList.add('task');
+	titleTask.innerText = value;
+	taskContent.appendChild(check);
+	taskContent.appendChild(titleTask);
+	//task.innerHTML = content;
+	const dateElement = document.createElement('spam');
+	dateElement.innerHTML = dateFormat;
+	task.appendChild(taskContent);
+	task.appendChild(dateElement);
+	task.appendChild(deleteIcon(id));
+	return task;
 };
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-import { addTask } from "./components/addTask.js";
-import { displayTasks } from "./components/readTasks.js";
-const btn = document.querySelector("[data-form-btn]");
+import { addTask } from './components/addTask.js';
+import { displayTasks } from './components/readTasks.js';
+const btn = document.querySelector('[data-form-btn]');
 
 //arrow functions o funciones anonimas
-btn.addEventListener("click", addTask);
+btn.addEventListener('click', addTask);
 displayTasks();
